7df8243f9c5820cfc806fbb134da53e84ff3886d
NickAllbritton <nallbritton@sycamores.indstate.edu>
2025-01-11 14:37:03 -0500

    added some comments outlining the plan

--- main.cpp
+++ main.cpp
@@ -10,14 +10,24 @@ static void activate(GtkApplication* app, gpointer user_data)
     gtk_window_present(GTK_WINDOW(window));
 }
 
+// try using GtkDrawingArea
+// This creates a canvas on which the user can draw. 
+
+// BIG PICTURE
+// 1. Create window with default size being maximized but with different settings options stored in XML
+// 2. Create a menu at the top file, view, edit
+// 3. Create a welcome screen for if the app was opened without giving a file to open
+// 4. Create notebook mode with a toolbar on the left and a notebook that is a canvas on which the user can write,type,insert pictures
+// 5. Profit? Not really but you get the idea.
+
 int main (int argc, char* argv[]) 
 {
-    GtkApplication *app;
+    GtkApplication* app;
     int status;
 
     app = gtk_application_new("beta.knowtebook", G_APPLICATION_DEFAULT_FLAGS);
-    g_signal_connect(app, "activate", G_CALLBACK (activate), NULL);
-    status = g_application_run(G_APPLICATION (app), argc, argv);
+    g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+    status = g_application_run(G_APPLICATION(app), argc, argv);
     g_object_unref(app);
 
     return status;
