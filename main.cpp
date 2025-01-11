9da6e304f5e9c382f16fda47d2f4e19b9181944d
NickAllbritton <nallbritton@sycamores.indstate.edu>
2025-01-11 14:18:54 -0500

    changed ugly formatting

--- main.cpp
+++ main.cpp
@@ -4,13 +4,14 @@ static void activate(GtkApplication* app, gpointer user_data)
 {
     GtkWidget *window;  
 
-    window = gtk_application_window_new (app);
-    gtk_window_set_title (GTK_WINDOW (window), "Know-tebook");
-    gtk_window_set_default_size (GTK_WINDOW (window), 400, 400);
-    gtk_window_present (GTK_WINDOW (window));
+    window = gtk_application_window_new(app);
+    gtk_window_set_title(GTK_WINDOW(window), "Know-tebook");
+    gtk_window_set_default_size(GTK_WINDOW(window), 400, 400);
+    gtk_window_present(GTK_WINDOW(window));
 }
 
-int main (int argc, char **argv) {
+int main (int argc, char* argv[]) 
+{
     GtkApplication *app;
     int status;
 
