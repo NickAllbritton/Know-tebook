1572bb39c067cb8032dbc86e36c900f344ca6859
NickAllbritton <nallbritton@sycamores.indstate.edu>
2025-01-11 14:44:05 -0500

    default size of window is maximized

--- main.cpp
+++ main.cpp
@@ -2,11 +2,11 @@
 
 static void activate(GtkApplication* app, gpointer user_data)
 {
-    GtkWidget *window;  
+    GtkWidget* window;  
 
     window = gtk_application_window_new(app);
     gtk_window_set_title(GTK_WINDOW(window), "Know-tebook");
-    gtk_window_set_default_size(GTK_WINDOW(window), 400, 400);
+    gtk_window_maximize(GTK_WINDOW(window));
     gtk_window_present(GTK_WINDOW(window));
 }
 
